bf30906c77c4b129227d0f3b0892d8f8d29a2029
Deison27tl <82671901+Deison27tl@users.noreply.github.com>
2023-06-23 08:01:26 -0500

    Ajustes
    Signed-off-by: Deison27tl <82671901+Deison27tl@users.noreply.github.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { ReservaFormComponent } from './components/reserva-form/reserva-form.component';
+import { ReservaComponent } from './components/reserva/reserva.component';
+import { EditarReservaComponent } from './components/editar-reserva/editar-reserva.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: LoginComponent, pathMatch: "full" },
+  { path: "login", component: LoginComponent, pathMatch: "full" },
+  { path: "reservas", component: ReservaComponent, pathMatch: "full" },
+  { path: "crearReserva", component: ReservaFormComponent, pathMatch: "full" },
+  { path: "editarReserva", component: EditarReservaComponent, pathMatch: "full" },
+
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    useHash: true
+  }
+  )],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { ReservaFormComponent } from './components/reserva-form/reserva-form.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ReservaComponent } from './components/reserva/reserva.component';
+import { EditarReservaComponent } from './components/editar-reserva/editar-reserva.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ReservaFormComponent,
+    ReservaComponent,
+    EditarReservaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
